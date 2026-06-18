02570624e1e9f758e12b1868b81122b531a4f87c
ss20827 <ss20827@nyu.edu>
2026-06-18 21:29:05 +0700

    done with while loops!

---
+++ for_loop_version.py
@@ -0,0 +1,23 @@
+def get_starting_number():
+    while True:
+        try:
+            n = int(input("How many bottles of beer on the wall? "))
+            if n >= 1:
+                return n
+        except:
+            pass
+
+def sing(start):
+    def bottle(n):
+        return "bottle" if n == 1 else "bottles"
+    for i in range(start, 0, -1):
+        if i > 1:
+            print(f"{i} {bottle(i)} of beer on the wall, {i} {bottle(i)} of beer. ")
+            print(f"Take one down, pass it around, {i - 1} {bottle(i - 1)} of beer on the wall. ")
+            print()
+        else:
+            print(f"1 bottle of beer on the wall, 1 bottle of beer. ")
+            print(f"Take it down, pass it around, no more bottles of beer on the wall! ")
+
+if __name__ == "__main__":
+    sing(get_starting_number())
